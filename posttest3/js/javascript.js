ce8f7ab2e4cbb58c3dee898fe48034d8795a71d7
Jihanjha63 <jihanhafizahariyani63@gmail.com>
2022-10-03 12:45:30 +0800

    posttest3

--- posttest3/js/javascript.js
+++ posttest3/js/javascript.js
@@ -1,18 +1,14 @@
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-  navbar.classList.toggle('active');
-}
-
-document.querySelector('#close-navbar').onclick = () =>{
-  navbar.classList.remove('active');
-}
-
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
   searchForm.classList.toggle('active');
 };
+
+let darkToggle = document.querySelector('#darkToggle');
+
+darkToggle.addEventListener('change', ()=> {
+  document.body.classList.toggle('dark');
+})
 
 window.onscroll = () =>{
   navbar.classList.remove('active');
@@ -34,88 +30,7 @@ function prev(){
   slides[index].classList.add('active');
 };
 
-var swiper = new Swiper(".products-slider", {
-  loop: true,
-  grabCursor : true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    550: {
-      slidesPerView: 2,
-    },
-    850: {
-      slidesPerView: 3,
-    },
-    1200: {
-      slidesPerView: 4,
-    },
-  },
-});
-
-var swiper = new Swiper(".arrivals-slider", {
-  loop: true,
-  grabCursor : true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    550: {
-      slidesPerView: 2,
-    },
-    850: {
-      slidesPerView: 3,
-    },
-    1200: {
-      slidesPerView: 4,
-    },
-  },
-});
-
-var swiper = new Swiper(".reviews-slider", {
-  loop: true,
-  grabCursor : true,
-  spaceBetween: 20,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-  loop: true,
-  grabCursor : true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    650: {
-      slidesPerView: 2,
-    },
-    1200: {
-      slidesPerView: 3,
-    },
-  },
-});
+const btnLogin = document.getElementById("btn-double");
+    btnLogin.addEventListener("dblclick", function(){
+      alert("You Haven't Save Anythings!");
+    });
